42c1379ae80bad24fe898d20a8ac97ddea70246a
gyukim1 <92254824+gyukim1@users.noreply.github.com>
2022-02-11 01:33:19 +0900

    Update ft_strtrim.c

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -12,20 +12,54 @@
 
 #include "libft.h"
 
-char	*ft_strtrim(char const *str, char const *set)
+static int	ft_c_vs_charset(char c, const char *cs)
 {
-	size_t	start;
-	size_t	end;
+	int	i;
 
-	start = 0;
-	end = ft_strlen(str);
-	if (str == NULL || set == NULL)
+	i = 0;
+	while (c != cs[i] && cs[i] && cs[i + 1] != '\0')
+		i++;
+	if (c == cs[i])
+		return (1);
+	else
 		return (0);
-	while (str[start] && ft_strchr(set, str[start]))
-		start++;
-	while (str[end - 1] && ft_strchr(set, str[end - 1]) && end > start)
-		end--;
-	if (start > end)
-		return (ft_strdup(""));
-	return (ft_substr(str, start, end - start));
+}
+
+static int	ft_quantite_trimer_debut(const char *s, const char *cs)
+{	
+	int	i;
+
+	i = 0;
+	while (s[i] && ft_c_vs_charset(s[i], cs))
+		i++;
+	return (i);
+}
+
+static int	ft_quantite_trimer_fin(const char *s, const char *cs)
+{	
+	int	i;
+	int	len;
+
+	len = ft_strlen(s);
+	i = len - 1;
+	while (ft_c_vs_charset(s[i], cs) && i > 0)
+		i--;
+	return (len - (i + 1));
+}
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	char	*dst;
+	int		taille;
+	int		i;
+	int		fin;
+
+	if (!s1)
+		return (NULL);
+	taille = ft_strlen(s1);
+	i = ft_quantite_trimer_debut(s1, set);
+	fin = ft_quantite_trimer_fin(s1, set);
+	taille = taille - fin - i;
+	dst = ft_substr(s1, i, taille);
+	return (dst);
 }
