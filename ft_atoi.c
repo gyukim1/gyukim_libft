ce623c101a0fad72dcd461580ff84675982657cb
gyukim1 <92254824+gyukim1@users.noreply.github.com>
2022-02-11 00:12:57 +0900

    Update ft_atoi.c

--- ft_atoi.c
+++ ft_atoi.c
@@ -14,13 +14,15 @@
 
 int	ft_atoi(const char *str)
 {
-	long long	res;
+	unsigned long long	res;
+	unsigned long long	max;
 	int				sign;
 	int				i;
 
 	sign = 1;
 	res = 0;
 	i = 0;
+	max = 9223372036854775807;
 	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
 		i++;
 	if (str[i] == 43 || str[i] == 45)
@@ -32,11 +34,11 @@ int	ft_atoi(const char *str)
 	while (str[i] >= 48 && str[i] <= 57)
 	{
 		res = (res * 10) + (str[i] - 48);
-		if (res * sign > 2147483647)
-			return (-1);
-		if (res * sign < -2147483648)
-			return (0);
 		i++;
 	}
+	if (result > max && sign == 1)
+		return (-1);
+	else if (result > max + 1 && sign == -1)
+		return (0);
 	return (res * sign);
 }
