c8860228b30eece3e7d65890e6c835e1555118af
gyukim1 <92254824+gyukim1@users.noreply.github.com>
2022-02-10 19:51:59 +0900

    Update ft_atoi.c

--- ft_atoi.c
+++ ft_atoi.c
@@ -32,9 +32,9 @@ int	ft_atoi(const char *str)
 	while (str[i] >= 48 && str[i] <= 57)
 	{
 		res = (res * 10) + (str[i] - 48);
-		if (res > 9223372036854775807 && pos == 1)
+		if (res > 9223372036854775807 && sign == 1)
 			return (-1);
-		if (res > 9223372036854775807 && pos == -1)
+		if (res > 9223372036854775807 && sign == -1)
 			return (0);
 		i++;
 	}
