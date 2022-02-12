3b0a655456b06d87d1d6f0a1cba29290eb087a28
gyukim1 <92254824+gyukim1@users.noreply.github.com>
2022-02-12 18:58:06 +0900

    Update ft_strtrim.c

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -14,21 +14,23 @@
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	char	*result;
-	size_t	s1_len;
+	char	*new_str;
+	size_t	start;
+	size_t	end;
 
-	result = 0;
-	if (!s1 || !set)
+	if (s1 == NULL)
 		return (NULL);
-	while (*s1 && ft_strchr(set, *s1))
-		s1++;
-	s1_len = ft_strlen(s1);
-	while (s1_len && s1[s1_len - 1] && ft_strchr(set, s1[s1_len - 1]))
-		s1_len--;
-	result = (char *)malloc(sizeof(char) * (s1_len + 1));
-	if (!result)
+	if (set == NULL)
+		return (ft_strdup(s1));
+	start = 0;
+	end = ft_strlen(s1);
+	while (s1[start] && ft_strchr(set, s1[start]))
+		start++;
+	while (end && s1[end - 1] && ft_strchr(set, s1[end - 1]))
+		end--;
+	new_str = (char *)malloc(sizeof(char) * (end - start) + 1);
+	if (!new_str)
 		return (NULL);
-	ft_memcpy(result, s1, s1_len);
-	result[s1_len] = '\0';
-	return (result);
+	ft_strlcpy(new_str, &s1[start], (end - start) + 1);
+	return (new_str);
 }
