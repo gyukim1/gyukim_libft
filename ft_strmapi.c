bef1e07a4f736b7d883537c5b4674c9047f88e19
gyukim1 <92254824+gyukim1@users.noreply.github.com>
2022-02-12 19:16:59 +0900

    Update ft_strmapi.c

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -14,18 +14,18 @@
 
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
-	size_t	length;
+	size_t	len;
 	size_t	i;
 	char	*str;
 
 	i = 0;
 	if (!s || !f)
 		return (0);
-	length = ft_strlen(s);
-	str = (char *)malloc(sizeof(char) * (length) + 1);
+	len = ft_strlen(s);
+	str = (char *)malloc(sizeof(char) * (len) + 1);
 	if (str == 0)
 		return (0);
-	while (i < length)
+	while (i < len)
 	{
 		str[i] = f(i, s[i]);
 		i++;
