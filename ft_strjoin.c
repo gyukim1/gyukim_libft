3c6e9af99003f4aba542808eb2b7ac653f9dcf5a
gyukim1 <92254824+gyukim1@users.noreply.github.com>
2022-02-12 19:23:03 +0900

    Update ft_strjoin.c

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -14,7 +14,7 @@
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	char	*res;
+	char	*new_str;
 	int		s1_len;
 	int		s2_len;
 
@@ -22,10 +22,10 @@ char	*ft_strjoin(char const *s1, char const *s2)
 		return (NULL);
 	s1_len = ft_strlen(s1);
 	s2_len = ft_strlen(s2);
-	res = (char *)malloc(sizeof(char) * (s1_len + s2_len + 1));
-	if (!res)
+	new_str = (char *)malloc(sizeof(char) * (s1_len + s2_len + 1));
+	if (!new_str)
 		return (NULL);
-	ft_strlcpy(res, s1, s1_len + 1);
-	ft_strlcat(res, s2, s1_len + s2_len + 1);
-	return (res);
+	ft_strlcpy(new_str, s1, s1_len + 1);
+	ft_strlcat(new_str, s2, s1_len + s2_len + 1);
+	return (new_str);
 }
